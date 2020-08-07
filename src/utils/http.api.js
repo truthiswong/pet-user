c0faed2dd35daff35e8392f38f56fdb40ef3b6b4
truthiswong <1457368987@qq.com>
2020-08-07 18:15:44 +0800

    封装api

--- src/utils/http.api.js
+++ src/utils/http.api.js
@@ -1,14 +1,31 @@
 const hostSso = 'http://pet-jw.app.iisu.cn/server/data'
-const hostUser = 'https://api.youzixy.com/'
-let hotSearchUrl = hostUser + 'ebapi/store_api/hot_search'
-let indexUrl = hostUser + 'ebapi/public_api/index'
+const hostUser = 'http://10.65.56.60:8381/'
+
+// let appointmentPage = hostUser + 'user/appointment/page' // 获取预约列表
+
+// let hospitalList = hostUser + 'hospital/hos/page' // 获取医院列表
+// let petList = hostUser + 'user/pet/search' // 获取宠物列表
+// let petTypeList = hostUser + 'user/pet/search' // 获取宠物类型列表
+// let serveList = hostUser + 'user/pet/service/item/date/setting/list' // 获取服务类型
+
+// let indexUrl = hostUser + 'ebapi/public_api/index'
 
 const install = (Vue, vm) => {
-	let getSearch = (params = {}) => vm.$u.get(hotSearchUrl, params)
-	let getInfo = (params = {}) => vm.$u.post(indexUrl, params)
+	let appointmentPage = (params = {}) => vm.$u.get(hostUser + 'user/appointment/page', params) // 获取预约列表
+	
+	let hospitalList = (params = {}) => vm.$u.get(hostUser + 'hospital/hos/page', params) // 获取医院列表
+	let petList = (params = {}) => vm.$u.get(hostUser + 'user/pet/search', params) // 获取宠物列表
+	let petTypeList = (params = {}) => vm.$u.get(hostUser + 'user/pet/search', params) // 获取宠物类型列表
+	let serveList = (params = {}) => vm.$u.get(hostUser + 'user/appointment/query', params) // 获取服务类型
+	let appointment = (params = {}) => vm.$u.post(hostUser + 'user/appointment/apply', params) // 申请预约
+
 	vm.$u.api = {
-		getSearch,
-		getInfo
+		appointmentPage,
+		hospitalList,
+		petList,
+		petTypeList,
+		serveList,
+		appointment
 	}
 }
 
--- src/utils/http.interceptor.js
+++ src/utils/http.interceptor.js
@@ -6,10 +6,12 @@ const install = (Vue, vm) => {
 		// 如果将此值设置为true，拦截回调中将会返回服务端返回的所有数据response，而不是response.data
 		// 设置为true后，就需要在this.$u.http.interceptor.response进行多一次的判断，请打印查看具体值
 		originalData: true,
+		loadingText: '努力加载中~',
+		loadingTime: 1500,
 		// 设置自定义头部content-type
 		header: {
 			'Authorization': vm.token,
-			'X-TENANT-ID': 'cuncun:cc@2020',
+			'X-TENANT-ID': 'jiawen:pethos@2020',
 			'Content-Type': 'application/x-www-form-urlencoded',
 		}
 	});
